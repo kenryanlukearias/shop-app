4fb64344ba230b74fcae1e432f959255e1c50887
kenryanlukearias <studyholicprocrastinator@gmail.com>
2025-06-07 23:57:31 +0400

    initial commit

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,27 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Prerender only static routes
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  // Use SSR for parameterized routes
+  {
+    path: 'edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'create',
+    renderMode: RenderMode.Prerender
+  },
+  // Fallback
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
