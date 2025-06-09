59fef304a7843e521237b5af7b039d3b641ec07b
kenryanlukearias <studyholicprocrastinator@gmail.com>
2025-06-09 23:18:10 +0400

    removed error

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { CustomerListComponent } from './components/customer-list/customer-list.
 import { CustomerCreateComponent } from './components/customer-create/customer-create.component';
 import { CustomerEditComponent } from './components/customer-edit/customer-edit.component';
 import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
-import { MainPageComponent } from './langing-page/main/main-page.component';
-import { LandingComponent } from './langing-page/landing/landing.component';
 
 export const routes: Routes = [
     
